e52e4465245f216b3d6eb1b7f2b6ca7af06d87df
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-03-09 23:49:35 +0900

    -

--- docs-src/src/App.tsx
+++ docs-src/src/App.tsx
@@ -1,11 +1,7 @@
-import { useDialogOpener } from './../../dist/index';
-import { Buttons } from './nestedDemo/Buttons';
 import { NestedDemo } from './nestedDemo/NestedDemo';
 import { ReturnDemo } from './returnDemo/ReturnDemo';
 
 export default function App() {
-  const { open } = useDialogOpener();
-
   return <div>
     <h1>React Light Dialog Demo</h1>
     <h2>Nested demo</h2>
