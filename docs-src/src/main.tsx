a5e881ca663f1c1bda21b40120ae4e3235714791
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-03-09 23:52:04 +0900

    -

--- docs-src/src/main.tsx
+++ docs-src/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { LightDialogProvider } from './../../dist/index';
-import './../../dist/style.css'
-import './index.css'
+import './../../dist/style.css';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
