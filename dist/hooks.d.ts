4fad7c660142663e266d32f081c8bf04e38ea432
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-03-09 20:22:43 +0900

    -

--- dist/hooks.d.ts
+++ dist/hooks.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react" />
 export declare function useDialogOpener(): {
-    open: (component: import("react").ReactNode, options?: import("./types").SimpleDialogOptions | undefined) => HTMLDialogElement;
+  open: (component: import("react").ReactNode, options?: import(".pnpm/react-simple-dialog@file+.._@emotion+css@11.13.5_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/react-simple-dialog/dist/types").SimpleDialogOptions | undefined) => HTMLDialogElement;
 };
 export declare function useDialogCloser(): {
-    close: () => void;
+  close: () => void;
 };
